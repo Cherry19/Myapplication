e6110beef51b70b18b10c44aa8de674fb0b8a289
Cherry19 <2984487900@qq.com>
2017-05-09 11:55:32 +0800

    Initial commit

---
+++ app/src/main/java/com/bignerdranch/android/zhangxiaojuan/zhangxiaojuanActivity.java
@@ -0,0 +1,13 @@
+package com.bignerdranch.android.zhangxiaojuan;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class zhangxiaojuanActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_zhangxiaojuan);
+    }
+}
